1ea04658b3cbaf2950ded5d38f9c42905aed2991
scott.palmer <scott.palmer@localhost>
2010-05-13 10:50:14 -0400

    Moved ComEnum to the COM4JNA project

---
+++ src/com/sun/jna/platform/win32/jnacom/ComEnum.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2009 Digital Rapids Corp.
+ * All rights reserved.
+ */
+
+package com.sun.jna.platform.win32.jnacom;
+
+/**
+ *
+ * @author scott.palmer
+ */
+public interface ComEnum {
+    public int comEnumValue();
+}
